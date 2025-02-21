8c346b4aef4a1873e0818bf42d8bb3ae6bc8b1a1
Alexius Huang <alexius.huang@gmail.com>
2025-02-21 23:57:34 +0100

    Setup prettier

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,16 @@
-import type { Route } from "./+types/home";
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Travel | Alexius Huang" },
-    { name: "description", content: "Alexius's Personal Travel Blog" },
-  ];
+    return [
+        { title: 'Travel | Alexius Huang' },
+        { name: 'description', content: "Alexius's Personal Travel Blog" },
+    ];
 }
 
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <p>
-        Coming Soon...!
-      </p>
-    </div>
-  );
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <p>Coming Soon...!</p>
+        </div>
+    );
 }
