4d2fe4c804a9855c33d1d81232512671a03ed4f2
Alexius <alexius.huang@gmail.com>
2025-06-23 20:43:38 +0200

    NOTICKET: Setup PostCSS & Font (#5)
    * Convert to normal route
    
    * Update css file
    
    * Install PostCSS Nesting
    
    * Update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 import { json } from '~/utils/response.server';
-import type { Route } from './+types/_index';
-import { Link, useLoaderData } from 'react-router';
+import type { Route } from './+types/index';
+import { Link } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -9,24 +9,34 @@ export function meta({}: Route.MetaArgs) {
     ];
 }
 
-interface LoaderData {
-    name: string;
-}
+interface LoaderData {}
 export async function loader(_: Route.LoaderArgs) {
-    const name = globalThis.__name;
-    globalThis.__name = '';
-    return json<LoaderData>({ name });
+    return json<LoaderData>({});
 }
 
+// const imageBaseUrl = 'https://images.alexius-huang.dev';
+// const BannerConfig = [
+//     {
+//         title: 'GREECE',
+//         date: [2024, 5],
+//         desktop: `${imageBaseUrl}/highlight/gr-oia-landscape.webp`,
+//         mobile: `${imageBaseUrl}/highlight/gr-santorini-vert-2.webp`
+//     },
+//     {
+//         title: 'EGYPT',
+//         date: [2024, 9],
+//         desktop: `${imageBaseUrl}/highlight/egp-gize-landscape.webp`,
+//         mobile: `${imageBaseUrl}/highlight/egp-gize-vert.webp`
+//     }
+// ];
+
 export default function Home() {
-    const { name } = useLoaderData<LoaderData>();
+    /* const {} = useLoaderData<LoaderData>(); */
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="mb-4 text-2xl">
-                {name
-                    ? `👋 Hello ${name} 👋`
-                    : '🚧 Website Currently Planning 🚧'}
+                🚧 Website Currently Planning 🚧
             </h1>
 
             <p>
