a9304d4b5178d85a042a7076304f66ddf2a63d27
Alexius Huang <alexius.huang@gmail.com>
2025-06-30 18:40:52 +0200

    NOTICKET: Update Theme Cookie Setting

--- app/utils/theme.server.ts
+++ app/utils/theme.server.ts
@@ -2,15 +2,15 @@ import { createThemeSessionResolver } from "remix-themes";
 import { createCookieSessionStorage } from "react-router";
 
 export const themeSessionResolver = createThemeSessionResolver(
-  createCookieSessionStorage({
-    cookie: {
-      name: "__remix-themes",
-      // domain: 'remix.run',
-      path: "/",
-      httpOnly: true,
-      sameSite: "lax",
-      secrets: ["s3cr3t"],
-      // secure: true,
-    },
-  }),
+    createCookieSessionStorage({
+        cookie: {
+            name: '__remix-themes',
+            domain: process.env.NODE_ENV === 'production' ? 'alexius-huang.dev' : undefined,
+            path: '/',
+            httpOnly: true,
+            sameSite: 'lax',
+            secrets: ['s3cr3t'],
+            secure: process.env.NODE_ENV === 'production',
+        },
+    }),
 );
