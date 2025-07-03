1daeb3146c8bbfbb7c23b2f849c249ad6c6f36e1
Alexius <alexius.huang@gmail.com>
2025-07-03 22:06:01 +0200

    ISSUE-21Host own Roboto and Chakra Petch font (#26)
    * Host own Roboto and Chakra Petch font
    
    * Prevent repomix on bundling the public directory

--- app/root.tsx
+++ app/root.tsx
@@ -29,18 +29,7 @@ import {
 
 import './app.css';
 
-export const links: Route.LinksFunction = () => [
-    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-    {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-        crossOrigin: 'anonymous',
-    },
-    {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Roboto:ital,wdth,wght@0,75..100,100..900;1,75..100,100..900&display=swap',
-    },
-];
+export const links: Route.LinksFunction = () => [];
 
 type LoaderData = {
     token: string;
