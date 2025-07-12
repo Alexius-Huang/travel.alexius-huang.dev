9cd890fd05eacfdb6b2eb852df24eff0b7545751
Alexius Huang <alexius.huang@gmail.com>
2025-07-12 14:25:17 +0200

    feat: use protomaps for serverless map tiles

--- app/containers/trip-details-page/map.tsx
+++ app/containers/trip-details-page/map.tsx
@@ -1,22 +1,41 @@
-// MapLibreMap.tsx
 import { useEffect, useRef, type FC } from 'react';
-import maplibregl from 'maplibre-gl';
+import maplibregl, { LngLat, LngLatBounds } from 'maplibre-gl';
+import { Protocol } from 'pmtiles';
+import mapStyle from './map-style.json';
 
 export const Map: FC = () => {
     const mapContainer = useRef<HTMLDivElement>(null);
+    const mapInstance = useRef<maplibregl.Map | null>(null);
 
     useEffect(() => {
         if (!mapContainer.current) return;
 
-        const map = new maplibregl.Map({
+        const protocol = new Protocol();
+        maplibregl.addProtocol('pmtiles', protocol.tile);
+
+        mapInstance.current = new maplibregl.Map({
             container: mapContainer.current,
-            style: '/toner-map-style.json',
-            center: [-74.006, 40.7128], // NYC
-            zoom: 13,
+            style: {
+                ...mapStyle,
+                sources: {
+                    protomaps: {
+                        type: "vector",
+                        url: "pmtiles://https://images.alexius-huang.dev/pmtiles/new-york.pmtiles"
+                    }
+                }
+            } as maplibregl.StyleSpecification,
+            maxBounds: new LngLatBounds([
+                new LngLat(-74.176123, 40.653505),
+                new LngLat(-73.771575, 40.868387),
+            ]),
+            center: [-74.006, 40.7128],
+            minZoom: 12,
+            maxZoom: 16,
         });
 
         return () => {
-            map.remove();
+            mapInstance.current?.remove();
+            maplibregl.removeProtocol('pmtiles');
         };
     }, []);
 
